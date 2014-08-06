769b778ea1597e562b131b9a02598518b89ec9ea
Florian BUREL <florian.burel@mac.com>
2014-08-06 16:08:04 +0200

    Ajout d'une delegation sur la classe CNCloudHelper

--- CloudNotes/CNCloudHelper.h
+++ CloudNotes/CNCloudHelper.h
@@ -8,10 +8,21 @@
 
 #import <Foundation/Foundation.h>
 
+@protocol CNCloudHelperDelegate <NSObject>
+
+// Called when files are found in iCloud
+- (void) cloudHelper:(id)sender didFindDocuments:(NSArray *)documents;
+
+@end
 @interface CNCloudHelper : NSObject
 
 + (instancetype)sharedInstance;
 
 - (void) retrieveUserAccountToken:(void(^)(BOOL hasAccount, BOOL accountHasChanged))completion;
+
+- (void) startMonitoringCloud;
+- (void) stopMonitoringCloud;
+
+@property (weak, nonatomic) id<CNCloudHelperDelegate> delegate;
 
 @end
